41796b992412f4e3c502e29b63ad9bb2a5606571
PrabhatTheCoder <117260172+PrabhatTheCoder@users.noreply.github.com>
2025-04-14 01:13:40 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,9 @@
 echo "Starting up the container..."
 
 echo "Applying migrations..."
-python manage.py makemigrations
+python manage.py makemigrations users
+python manage.py makemigrations app
+python manage.py makemigrations django_celery_beat
 python manage.py migrate
 
 echo "Collecting static files..."
